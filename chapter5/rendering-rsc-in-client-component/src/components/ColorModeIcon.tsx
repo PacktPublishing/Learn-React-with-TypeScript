b16b6401186143731dc30c68dcdfc07fbcfdfffb
Carl <carl.rippon@googlemail.com>
2024-12-13 15:00:14 +0000

    chapter 5 updates

--- chapter5/rendering-rsc-in-client-component/src/components/ColorModeIcon.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-import colorModeSvg from './colorModeIcon.svg';
-
-export function ColorModeIcon() {
-  console.log('Is ColorModeIcon a RSC?');
-  return <Image src={colorModeSvg} alt="Color Mode Icon" className="mr-2 h-6 w-6" />;
-}
